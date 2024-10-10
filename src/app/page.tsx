4624b3d18a8a75a961282496a90dc0ead17272c6
Inggis Kurnia Trisiawan <inggiskurnia32@gmail.com>
2024-10-10 11:35:38 +0700

    chore: fix aposthrope

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <h1 className="text-[#F3CF56] text-4xl font-bold mb-[17px]" style={{WebkitTextStroke: '1px #4A7582'}}>emojilogy</h1>
       <div className="bg-[#F8DD84] flex-grow flex flex-col items-center pt-[100px]">
           <div className="text-5xl font-bold text-[#4C9BB9] text-center">
-            <h1> What's your</h1>
+            <h1> What&apos;s your</h1>
             <h1> <span className="text-[#1C5469] font-extrabold">emoji</span> today?</h1>
           </div>
             <Emoji></Emoji>
